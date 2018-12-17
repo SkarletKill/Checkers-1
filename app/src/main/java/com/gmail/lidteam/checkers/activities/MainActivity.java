4c577876d9968afeca5c3115207859a245eaa260
danarossa <37446099+danarossa@users.noreply.github.com>
2018-12-17 23:33:43 +0200

    i edited the drawer meny icons

--- app/src/main/java/com/gmail/lidteam/checkers/activities/MainActivity.java
+++ app/src/main/java/com/gmail/lidteam/checkers/activities/MainActivity.java
@@ -79,15 +79,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_start_offline) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_start_bluetooth) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_start_online) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_intro) {
 
         } else if (id == R.id.nav_send) {
 
