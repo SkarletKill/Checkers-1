69256c78a56ed63597d5205a274763d40fe70ac3
NeO <Skarletvlad@gmail.com>
2018-12-22 20:19:07 +0200

    - added timer on OneGameActivity

--- app/src/main/java/com/gmail/lidteam/checkers/activities/OneGameActivity.java
+++ app/src/main/java/com/gmail/lidteam/checkers/activities/OneGameActivity.java
@@ -3,13 +3,11 @@ package com.gmail.lidteam.checkers.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
-import com.gmail.lidteam.checkers.adapters.DataAdapter;
 import com.gmail.lidteam.checkers.R;
 import com.gmail.lidteam.checkers.adapters.ImageAdapter;
 import com.gmail.lidteam.checkers.controllers.GameController;
@@ -17,8 +15,9 @@ import com.gmail.lidteam.checkers.models.CheckerColor;
 import com.gmail.lidteam.checkers.models.CheckerType;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
-public class OneGameActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class OneGameActivity extends AppCompatActivity {
     private TextView username;
     private TextView opponent;
     private Timer timer;
@@ -31,14 +30,13 @@ public class OneGameActivity extends AppCompatActivity implements AdapterView.On
     private GameController gameController;
 
     private TextView mSelectText;
-    private DataAdapter mAdapter;
 
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_game);
 
+        createTimer();
         mSelectText = (TextView) findViewById(R.id.game_type);
         GridView gridview = (GridView) findViewById(R.id.gridView1);
         gridview.setAdapter(new ImageAdapter(this));
@@ -57,28 +55,64 @@ public class OneGameActivity extends AppCompatActivity implements AdapterView.On
         }
     };
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View v, int position,
-                               long id) {
-        mSelectText.setText("Выбранный элемент: " + mAdapter.getItem(position));
+    private String fixTime(String time) {
+        return (time.length() < 2) ? "0" + time : time;
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        mSelectText.setText("Выбранный элемент: ничего");
+    private void createTimer() {
+        final int[] seconds = {0};
+        final int[] minutes = {0};
+
+        //Declare the timer
+        Timer t = new Timer();
+        //Set the schedule function and rate
+        t.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        TextView tv = (TextView) findViewById(R.id.timer);
+                        tv.setText(fixTime(String.valueOf(minutes[0])) + " : " + fixTime(String.valueOf(seconds[0])));
+                        seconds[0] += 1;
+
+                        if (seconds[0] == 60) {
+                            tv.setText(fixTime(String.valueOf(minutes[0])) + " : " + fixTime(String.valueOf(seconds[0])));
+
+                            seconds[0] = 0;
+                            minutes[0] = minutes[0] + 1;
+                        }
+                    }
+
+                });
+            }
+
+        }, 0, 1000);
     }
 
-    public void onGridItemClick(){}
-    public void onSurrenderButtonClick(){}
+    public void onGridItemClick() {
+    }
 
-    public void repaintDesk(){} // ???
-    public void addChecker(String coordinates, CheckerColor color, CheckerType type){}
-    public void deleteChecker(String coordinates){}
-    public void selectCell(String coordinates){
+    public void onSurrenderButtonClick() {
+    }
+
+    public void repaintDesk() {
+    } // ???
+
+    public void addChecker(String coordinates, CheckerColor color, CheckerType type) {
+    }
+
+    public void deleteChecker(String coordinates) {
+    }
+
+    public void selectCell(String coordinates) {
         // change color
     }
-    public void changeBackgroundColor(){}
 
+    public void changeBackgroundColor() {
+    }
 
 
 }
